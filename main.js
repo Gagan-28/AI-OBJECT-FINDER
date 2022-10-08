7352eb258ce978d006d3deaf84e97004e9e9af92
Gagan-28 <84262680+Gagan-28@users.noreply.github.com>
2022-10-08 11:54:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+
+function preload()
+{
+    canvas = createCanvas(540, 420);
+    canvas.center();
+
+    
+}
+
+function setup()
+{
+    
+}
+
+function modelLoaded()
+{
+    
+}
+
+function gotResult()
+{
+    
+}
+
+function draw()
+{
+    
+}
